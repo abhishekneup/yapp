10943c2bb390f09fa8ac88fd7584111bda548342
abhishekneup <neupaneabhishek01@gmail.com>
2024-02-07 23:36:31 +0545

    ColorSelector

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Card from './card/card';
+import SelectColor from './color selector/Select_color';
 import Counter from './counter/counter';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <h1 className="bg-red-300 text-black p-4 m-20 rounded-md font-serif">HEllo World</h1>
       <Counter/>
       <Card/>
+      <SelectColor/>
     </div>
   );
 }
---
+++ src/color selector/Select_color.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+
+ function SelectColor () {
+
+    const [color, setColor] = useState("red");
+    return(
+        <div className=" w-full h-screen flex justify-center align-middle "  style={{backgroundColor: color}}>
+            <div className=" bg-slate-400 w-full h-12 rounded-md flex justify-center items-center">
+                <button className="bg-red-300"  onClick={() => setColor("red")}>Red</button>
+                <button className="ml-4 bg-green-300" onClick={()=> setColor("green")} >Green</button>
+                <button className="ml-4 bg-yellow-300" onClick={()=> setColor("yellow")} >Yellow</button>
+
+
+
+        </div>
+        </div>
+    )
+}
+
+export default SelectColor
